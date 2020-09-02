004677cc4869899658b596408c401850e5c9c4b4
Cezary Pokropek <cezary.pokropek@gmail.com>
2020-09-02 22:17:49 +0200

    pre-implementing Dialog logic

--- TaskTimerAndroidJava/app/src/main/java/cezary/pokropek/tasktimer/AppDialog.java
+++ TaskTimerAndroidJava/app/src/main/java/cezary/pokropek/tasktimer/AppDialog.java
@@ -1,11 +1,24 @@
 package cezary.pokropek.tasktimer;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class AppDialog extends DialogFragment {
     public static final String TAG = "AppDialog";
+
+    public static final String DIALOG_ID = "id";
+    public static final String DIALOG_MESSAGE = "message";
+    public static final String DIALOG_POSITIVE_RID = "positive_rid";
+    public static final String DIALOG_NEGATIVE_RID = "negative_rid";
+
 
     /**
      * The dialogue's callback interface to notify of user selected results (deletions confirmaed, etc.).
@@ -16,5 +29,58 @@ public class AppDialog extends DialogFragment {
         void onDialogCancelled(int dialogId);
     }
 
+    private DialogEvents mDialogEvents;
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        Log.d(TAG, "onAttach: Entering onAttach, activity is " + context.toString());
+        super.onAttach(context);
+
+        // Activities containing this fragment must implement its callbacks.
+        if(!(context instanceof DialogEvents)){
+            throw new ClassCastException(context.toString() + "must implement AppDialog.DialogEvent interface");
+        }
+        mDialogEvents = (DialogEvents) context;
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach: Entering...");
+        super.onDetach();
+
+        // Reset the active callbacks interface, because we don't have an activity longer.
+        mDialogEvents = null;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateDialog: starts");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        final Bundle arguments = getArguments();
+        final int dialogId;
+        String messageString;
+        int positiveStringId;
+        int negativeStringId;
+
+        builder.setMessage(messageString)
+                .setPositiveButton(positiveStringId, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        // callback positive result method
+                        mDialogEvents.onPositiveDialogResult(dialogId, arguments);
+                    }
+                })
+                .setNegativeButton(negativeStringId, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        // callback negative result method
+                        mDialogEvents.onNegativeDialogResult(dialogId, arguments);
+                    }
+                });
+
+        return super.onCreateDialog(savedInstanceState);
+    }
 }
